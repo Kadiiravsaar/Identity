7208f8554bca4ec8528e6afad4317baea26a31f7
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-18 15:31:39 +0300

    Models in AppDbContext added

---
+++ AspNetCoreIdentityApp.Web/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityApp.Web.Models
+{
+    public class AppDbContext : IdentityDbContext<AppUser,AppRole,string> // string ilgili kullanıcılar için random quid üreticek
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) // connection stringi program.cs de tutmak için
+        {
+            
+        }
+    }
+}
---
+++ AspNetCoreIdentityApp.Web/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentityApp.Web.Models
+{
+    public class AppUser : IdentityUser
+    {
+        // kullanıcı hakkında ek bilgiler tutulabilir
+        //public string City { get; set; }
+    }
+}
